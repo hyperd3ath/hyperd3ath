071da858ccb18339cad2749e0d3f21789da5446d
hyperd3ath official account <119930180+hyperd3ath@users.noreply.github.com>
2022-12-07 22:20:40 +0000

    Update hyperd3ath.sh

--- hyperd3ath.sh
+++ hyperd3ath.sh
@@ -1,37 +1,13 @@
-#!/bin/sh
-
-echo 'Welcome to hyperd3ath v1.1-alpha.'
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+#!/bin/bash
+echo 'Welcome to hyperd3ath v1.2-alpha.'
 
 dsenableroot 
 echo 'root user hopefully enabled and on login screen.'
 echo 'installing brew. may fail on 10.13, git issues lol'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-else if [[ ! REPLY =~ ^[Nn]$ ]]
- exit -1
-
-
-read -p "Would you like to disable SIP? (y/n) " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
  
 echo 'a restart is needed. when your computer shuts down fully, press CMD + R until you boot into recovery mode. from there, in the terminal, run "csrutil disable" without the quotation marks.'
 
-else if [[ ! REPLY =~ ^[Nn]$ ]]
- exit -1
-
-read -p "type Y when ready to reboot " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-
-echo 'enter password ofc'
+ read -s -n 1 -p "Press any key to continue . . ."
 sudo shutdown -r now
 
-else if [[ ! REPLY =~ ^[Nn]$ ]]
- exit -1
