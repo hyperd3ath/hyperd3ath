cb4dd930258efa37c3d365f9ee6396d8ecab4558
hyperd3ath official account <119930180+hyperd3ath@users.noreply.github.com>
2022-12-07 19:50:10 +0000

    elsa file

--- hyperd3ath.sh
+++ hyperd3ath.sh
@@ -11,6 +11,9 @@ echo 'root user hopefully enabled and on login screen.'
 echo 'installing brew. may fail on 10.13, git issues lol'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+else
+fi
+
 
 read -p "Would you like to disable SIP? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
@@ -18,6 +21,9 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
  
 echo 'a restart is needed. when your computer shuts down fully, press CMD + R until you boot into recovery mode. from there, in the terminal, run "csrutil disable" without the quotation marks.'
+
+else
+fi
 
 read -p "type Y when ready to reboot " -n 1 -r
 echo    # (optional) move to a new line
@@ -27,6 +33,8 @@ then
 echo 'enter password ofc'
 sudo shutdown -r now
 
+else
+fi
 
 
 
