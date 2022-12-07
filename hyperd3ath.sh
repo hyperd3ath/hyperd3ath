daabfe0d4a204e90fc628146d50b57eed4951751
hyperd3ath official account <119930180+hyperd3ath@users.noreply.github.com>
2022-12-07 20:03:05 +0000

    Update hyperd3ath.sh

--- hyperd3ath.sh
+++ hyperd3ath.sh
@@ -11,7 +11,7 @@ echo 'root user hopefully enabled and on login screen.'
 echo 'installing brew. may fail on 10.13, git issues lol'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-else
+else if [[ ! REPLY =~ ^[Nn]$ ]]
  exit -1
 fi
 
@@ -23,7 +23,7 @@ then
  
 echo 'a restart is needed. when your computer shuts down fully, press CMD + R until you boot into recovery mode. from there, in the terminal, run "csrutil disable" without the quotation marks.'
 
-else
+else if [[ ! REPLY =~ ^[Nn]$ ]]
  exit -1
 fi
 
@@ -35,6 +35,6 @@ then
 echo 'enter password ofc'
 sudo shutdown -r now
 
-else
+else if [[ ! REPLY =~ ^[Nn]$ ]]
  exit -1
 fi
