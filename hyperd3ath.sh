8b0bda952ab221f7842e339ef517b2e62dbfd632
hyperd3ath official account <119930180+hyperd3ath@users.noreply.github.com>
2022-12-07 19:39:15 +0000

    try fix stuff

--- hyperd3ath.sh
+++ hyperd3ath.sh
@@ -1,40 +1,28 @@
 #!/bin/sh
 
-echo 'Welcome to hyperd3ath v1-alpha.'
-read -p "Do you want to proceed? (yes/no) " yn
-
-case $yn in 
-	yes ) ok, proceeding...
-	no ) echo exiting...;
-		exit;;
-	* ) echo invalid response;
-		exit 1;;
-esac
-
+echo 'Welcome to hyperd3ath v1.1-alpha.'
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
 
 dsenableroot 
 echo 'root user hopefully enabled and on login screen.'
-echo 'installing brew.'
+echo 'installing brew. may fail on 10.13, git issues lol'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";;
 
-read -p "Do you want to disable SIP? (yes/no) " yn
 
-case $yn in 
-	yes ) echo 'ok, proceeding...';;
-	no ) echo exiting...;
-		exit;;
-	* ) echo 'invalid response';
-		exit 1;;
-esac
-
+read -p "Would you like to disable SIP? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+ 
 echo 'a restart is needed. when your computer shuts down fully, press CMD + R until you boot into recovery mode. from there, in the terminal, run "csrutil disable" without the quotation marks.'
-read -p "type yes to reboot when ready." yn
 
-case $yn in 
-	yes ) echo 'ok, proceeding...';;
-	* ) echo 'invalid response';
-		exit 1;;
-esac
+read -p "type Y when ready to reboot " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
 
 echo 'enter password ofc'
 sudo shutdown -r now
